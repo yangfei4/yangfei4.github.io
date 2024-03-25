0a59b48279627c2ca7d2e83cea3520a1f9ce31aa
D-YF <daiyf1028@gmail.com>
2024-03-24 21:08:46 -0500

    updated self-intro

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <p className="about-paragraph">
                         I am a Master's MechSE student at University of Illinos Urbana-Champaign, 
                         specializing in software development(full-stack) within a diverse background encompassing machine learning and robotics.
-                        I'm actively seeking a full-time <b>Software Development Engineer</b> (SDE) position for Summer 2024.
+                        I'm actively seeking <b>Software Development Engineer</b> (SDE) internship for Summer 2024.
                     </p>
                 </div>
                 <div className="about-image">
